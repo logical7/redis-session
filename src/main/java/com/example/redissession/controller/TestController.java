908d9db9173bacf27a5bc768b59a15d408545a11
admin <462542376@qq.com>
2021-09-25 15:03:26 +0800

    增加测试接口

---
+++ src/main/java/com/example/redissession/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.redissession.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author yinjun
+ */
+@RestController
+@RequestMapping(value = "/v1")
+public class TestController {
+    @GetMapping(value = "/test")
+    public String test() {
+        return "test domo--" + new Date().toString();
+    }
+}

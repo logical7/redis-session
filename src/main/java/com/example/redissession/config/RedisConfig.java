19edf049ef7546e688ee811e45bed269d23be07c
admin <462542376@qq.com>
2021-09-25 10:39:57 +0800

    first

---
+++ src/main/java/com/example/redissession/config/RedisConfig.java
@@ -0,0 +1,15 @@
+//package com.example.redissession.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//
+//@Configuration
+//public class RedisConfig {
+//    public RedisConnectionFactory redisConnectionFactory(){
+//        RedisStandaloneConfiguration redisStandaloneConfiguration=
+//                new RedisStandaloneConfiguration("47.114.188.142",6379);
+//        return new JedisConnectionFactory(redisStandaloneConfiguration);
+//    }
+//}
